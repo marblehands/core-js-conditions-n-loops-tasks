12b0d0772624f34da5e7432c3461a421e2e71510
Anna Chebysheva <anna.chebysheva@gmail.com>
2024-01-02 17:04:18 +0100

    feat: canQueenCaptureKing task is done

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -67,8 +67,17 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (king.x === queen.x || king.y === queen.y) {
+    return true;
+  }
+  if (
+    Math.abs(queen.y + (queen.x - king.x)) === king.y ||
+    Math.abs(queen.y - (queen.x - king.x)) === king.y
+  ) {
+    return true;
+  }
+  return false;
 }
 
 /**
